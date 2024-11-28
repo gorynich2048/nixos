ec9115a13697ed82f369c4c1708b07812ff75e5e
gorynich <terr2048@gmail.com>
2024-11-28 21:38:03 +0300

    nixvim: disable dap-ui

--- modules/nixvim/plugins/dap.nix
+++ modules/nixvim/plugins/dap.nix
@@ -4,7 +4,7 @@
       dap = {
         enable = true;
         extensions = {
-          # dap-virtual-text.enable = true;
+          dap-virtual-text.enable = true;
           dap-ui = {
             enable = true;
             controls.enabled = false;
@@ -22,20 +22,20 @@
         };
       };
     };
-    extraConfigLua = ''
-      local dap, dapui = require("dap"), require("dapui")
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    '';
+    # extraConfigLua = ''
+    #   local dap, dapui = require("dap"), require("dapui")
+    #   dap.listeners.before.attach.dapui_config = function()
+    #     dapui.open()
+    #   end
+    #   dap.listeners.before.launch.dapui_config = function()
+    #     dapui.open()
+    #   end
+    #   dap.listeners.before.event_terminated.dapui_config = function()
+    #     dapui.close()
+    #   end
+    #   dap.listeners.before.event_exited.dapui_config = function()
+    #     dapui.close()
+    #   end
+    # '';
   };
 }
