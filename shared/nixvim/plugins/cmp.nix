2a34822e1dfb0a32538a58f422be7082d854b87c
gorynich <terr2048@gmail.com>
2025-01-27 16:48:42 +0300

    nixvim: remove luasnip

--- shared/nixvim/plugins/cmp.nix
+++ shared/nixvim/plugins/cmp.nix
@@ -1,13 +1,10 @@
 {
   programs.nixvim = {
     plugins = {
-      luasnip.enable = true;
       cmp = {
         enable = true;
 
         settings = {
-          snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-
           mapping = {
             "<C-u>" = "cmp.mapping.scroll_docs(-4)";
             "<C-d>" = "cmp.mapping.scroll_docs(4)";
@@ -21,7 +18,6 @@
           sources = [
             {name = "path";}
             {name = "nvim_lsp";}
-            {name = "luasnip";}
             # {
             #   name = "buffer";
             #   # Words from other open buffers can also be suggested.
