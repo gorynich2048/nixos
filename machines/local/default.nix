9ee04e66cb90e6f8d2a1ca6432e3c2763ddba3a5
gorynich <terr2048@gmail.com>
2025-03-22 19:01:35 +0300

    qemu networking experiments

--- machines/local/default.nix
+++ machines/local/default.nix
@@ -20,6 +20,37 @@
       130.61.52.25 xcore1
       176.9.86.158 host
     '';
+    nat = {
+      enable = true;
+      internalIPs = [ "192.168.100.0/24" ];
+      externalInterface = "enp4s0";
+    };
+  };
+
+  systemd.network = {
+    enable = true;
+    netdevs = {
+      "10-br0" = {
+        netdevConfig = {
+          Kind = "bridge";
+          Name = "br0";
+        };
+      };
+    };
+    networks = {
+      "20-taps" = {
+        matchConfig.Name ="tap*";
+        networkConfig = {
+          Bridge = "br0";
+        };
+      };
+      "20-br0" = {
+        matchConfig.Name = "br0";
+        networkConfig = {
+          Address = [ "192.168.100.1/24" ];
+        };
+      };
+    };
   };
 
   security.rtkit.enable = true;
--- machines/vpn/default.nix
+++ machines/vpn/default.nix
@@ -14,10 +14,27 @@
   };
 
   virtualisation.vmVariant = {
+    systemd.network = {
+      enable = true;
+      networks."10-eth" = {
+        matchConfig.Type = "ether";
+        # matchConfig.MACAddress = "52:55:00:d1:55:01";
+        networkConfig = {
+          Address = [ "192.168.100.2/24" ];
+          Gateway = "192.168.100.1";
+          DHCP = "no";
+        };
+      };
+    };
     virtualisation = {
       graphics = false;
+      qemu.networkingOptions = [
+        "-netdev tap,id=nd0,ifname=tap0,script=no,downscript=no"
+        "-device virtio-net-pci,netdev=nd0,mac=52:55:00:d1:55:01"
+      ];
     };
   };
+  users.users.root.password = "test";
 
   system.stateVersion = "24.05"; # NEVER CHANGE
   home-manager = {
