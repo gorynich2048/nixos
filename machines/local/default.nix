3af0efc977945b46c0aeb9c55946ae63c19675e1
gorynich <terr2048@gmail.com>
2025-03-22 20:26:54 +0300

    qemu: configure interfaces for 16 vms ahead of time

--- machines/local/default.nix
+++ machines/local/default.nix
@@ -30,28 +30,30 @@
   systemd.network = {
     enable = true;
     networks = let
-      index = 1;
-    in {
-      "10-vm${toString index}" = {
-        matchConfig.Name = "vm${toString index}";
-        address = [
-          "192.168.100.0/32"
-          "fec0::/128"
-        ];
-        routes = [
-          {
-            Destination = "192.168.100.${toString index}/32";
-          }
-          {
-            Destination = "fec0::${lib.toHexString index}/128";
-          }
-        ];
-        networkConfig = {
-          IPv4Forwarding = true;
-          IPv6Forwarding = true;
-        };
-      };
-    };
+      maxVMs = 16;
+    in builtins.listToAttrs (
+        map (index: {
+          name = "10-vm${toString index}";
+          value = {
+            matchConfig.Name = "vm${toString index}";
+            # Host's addresses
+            address = [
+              "192.168.100.0/32"
+              "fec0::/128"
+            ];
+            # Setup routes to the VM
+            routes = [
+              { Destination = "192.168.100.${toString index}/32"; }
+              { Destination = "fec0::${lib.toHexString index}/128"; }
+            ];
+            # Enable routing
+            networkConfig = {
+              IPv4Forwarding = true;
+              IPv6Forwarding = true;
+            };
+          };
+        }) (lib.genList (i: i + 1) maxVMs)
+      );
   };
 
   security.rtkit.enable = true;
