6592862531bc77f73dc1cdb3a727edaa740cda6f
gorynich <terr2048@gmail.com>
2025-03-23 18:02:37 +0300

    vpn-vm: enable bootloader

--- machines/vpn/default.nix
+++ machines/vpn/default.nix
@@ -1,55 +1,61 @@
-{ lib, ... }: {
+{ lib, ... }:
+let
+  index = 1;
+  mac = "00:00:00:00:00:01";
+in {
   imports = [
     ../../modules/shared
     ../../modules/remote
+
+    ./hardware-configuration.nix
   ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
   networking = {
     hostName = "vpn";
+    useDHCP = false;
   };
 
-  virtualisation.vmVariant = let
-    index = 1;
-    mac = "00:00:00:00:00:01";
-  in {
-    networking.useDHCP = false;
-    systemd.network = {
-      enable = true;
-      networks."10-eth" = {
-        matchConfig.MACAddress = mac;
-        address = [
-          "192.168.100.${toString index}/32"
-          "fec0::${lib.toHexString index}/128"
-        ];
-        routes = [
-          {
-            # A route to the host
-            Destination = "192.168.100.0/32";
-            GatewayOnLink = true;
-          }
-          {
-            # Default route
-            Destination = "0.0.0.0/0";
-            Gateway = "192.168.100.0";
-            GatewayOnLink = true;
-          }
-          {
-            # Default route
-            Destination = "::/0";
-            Gateway = "fec0::";
-            GatewayOnLink = true;
-          }
-        ];
-        linkConfig.RequiredForOnline = "routable";
-      };
-    };
-    virtualisation = {
-      graphics = false;
-      qemu.networkingOptions = lib.mkForce [
-        "-netdev tap,id=nd0,ifname=vm${toString index},script=no,downscript=no"
-        "-device virtio-net-pci,netdev=nd0,mac=${mac}"
+  systemd.network = {
+    enable = true;
+    networks."10-eth" = {
+      matchConfig.MACAddress = mac;
+      address = [
+        "192.168.100.${toString index}/32"
+        "fec0::${lib.toHexString index}/128"
       ];
+      routes = [
+        {
+          Destination = "192.168.100.0/32";
+          GatewayOnLink = true;
+        }
+        {
+          Destination = "0.0.0.0/0";
+          Gateway = "192.168.100.0";
+          GatewayOnLink = true;
+        }
+        {
+          Destination = "::/0";
+          Gateway = "fec0::";
+          GatewayOnLink = true;
+        }
+      ];
+      linkConfig.RequiredForOnline = "routable";
     };
+  };
+
+  virtualisation.vmVariantWithBootLoader.virtualisation = {
+    qemu.networkingOptions = lib.mkForce [
+      "-netdev tap,id=nd0,ifname=vm${toString index},script=no,downscript=no"
+      "-device virtio-net-pci,netdev=nd0,mac=${mac}"
+    ];
+    cores = 4;
+    memorySize = 16384;
+    diskSize = 65536;
   };
 
   system.stateVersion = "24.05"; # NEVER CHANGE
