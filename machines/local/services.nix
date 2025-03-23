a2c71148a5977c768ab64d5a55bdbb5a64f2b36d
gorynich <terr2048@gmail.com>
2025-03-24 01:34:34 +0300

    pipewire: lock mic volume

--- machines/local/services.nix
+++ machines/local/services.nix
@@ -1,10 +1,23 @@
-{ pkgs, ... }: {
+{ ... }: {
   services = {
     pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+
+      extraConfig.pipewire-pulse."10-lock-mic-volume" = {
+        "pulse.rules" = [
+          {
+            matches = [
+              { "application.id" = "!org.PulseAudio.pavucontrol"; }
+            ];
+            actions = {
+              quirks = [ "block-source-volume" ];
+            };
+          }
+        ];
+      };
     };
   };
 }
