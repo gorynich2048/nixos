452e9666db98413c9cbe553800ba4958231c9a87
gorynich <terr2048@gmail.com>
2025-03-23 20:02:23 +0300

    vpn-vm: fix boot as systemd service

--- machines/host/services.nix
+++ machines/host/services.nix
@@ -1,10 +1,16 @@
 { self, ... }: {
   systemd.services = {
     vpn-vm = {
-      wantedBy = [ "default.target" ];
-      serviceConfig = {
-        ExecStart = "${self.nixosConfigurations.vpn.config.system.build.vmWithBootLoader}/bin/run-vpn-vm";
+      wantedBy = [ "multi-user.target" ];
+      environment = {
+        KVM_NAME = "vpn";
       };
+      script = ''
+        VM_STORAGE=/var/vm/$KVM_NAME
+        mkdir -p $VM_STORAGE
+        cd $VM_STORAGE
+        ${self.nixosConfigurations.vpn.config.system.build.vmWithBootLoader}/bin/run-$KVM_NAME-vm
+      '';
     };
   };
 }
