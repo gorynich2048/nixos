046013d9ac17b8d895fc805f8b91ff58fcb2744d
gorynich <terr2048@gmail.com>
2025-03-25 21:55:16 +0300

    vpn: add phone

--- modules/wireguard/server.nix
+++ modules/wireguard/server.nix
@@ -30,6 +30,10 @@
           publicKey = "/4GA4n7riAxw0ffnegSUrfx2Xz98HTU49zSfKCwuEh0=";
           allowedIPs = [ "10.0.0.2/32" ];
         }
+        {
+          publicKey = "bfr3Q/hkv9oJ4kzcQ9cvwVKxeHiyPo9tRRO+ETAEoxo=";
+          allowedIPs = [ "10.0.0.3/32" ];
+        }
       ];
     };
   };
