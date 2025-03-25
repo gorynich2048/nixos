349d3705d28706473a76a5993ef46a882955093d
gorynich <terr2048@gmail.com>
2025-03-25 22:48:38 +0300

    vpn: refactor nat rules

--- modules/wireguard/server.nix
+++ modules/wireguard/server.nix
@@ -1,12 +1,13 @@
-{ pkgs, ... }: {
+{ ... }: {
   imports = [
     ./amnezia.nix
     ./genkey.nix
   ];
 
-  networking.nat.enable = true;
-  networking.nat.externalInterface = "eth0";
-  networking.nat.internalInterfaces = [ "wg0" ];
+  networking.nat = {
+    enable = true;
+    internalIPs = [ "10.0.0.0/24" ];
+  };
   networking.firewall.allowedUDPPorts = [ 62048 ];
 
   networking.wireguard.enable = true;
@@ -14,16 +15,6 @@
     wg0 = {
       ips = [ "10.0.0.1/24" ];
       listenPort = 62048;
-
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
-      '';
-
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
-      '';
 
       peers = [
         {
