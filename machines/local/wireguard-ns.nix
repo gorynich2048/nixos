5e279f99d5c2885799fb672c5b5977cc6c51c3df
gorynich <terr2048@gmail.com>
2025-03-26 00:44:13 +0300

    refactor

--- machines/local/wireguard-ns.nix
+++ machines/local/wireguard-ns.nix
@@ -7,14 +7,14 @@
       in {
         Type = "oneshot";
         RemainAfterExit = "yes";
-        ExecStart = with pkgs; writers.writeBash "wg-ns-up" ''
+        ExecStart = pkgs.writers.writeBash "wg-ns-up" ''
           ${ip} netns add wg
           ${ip} link add link enp4s0 name en0 netns wg type macvlan
           ${ip} -n wg link set en0 up
           ${ip} -n wg addr add 192.168.1.200/24 dev en0
           ${ip} -n wg route add 176.9.86.158 via 192.168.1.1
         '';
-        ExecStop = with pkgs; writers.writeBash "wg-ns-down" ''
+        ExecStop = pkgs.writers.writeBash "wg-ns-down" ''
           ${ip} netns del wg
         '';
       };
