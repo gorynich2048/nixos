bc9091153e5db7f9edd6c003a23cce6a48773008
gorynich <terr2048@gmail.com>
2025-03-31 12:33:50 +0300

    tmux fixes

--- home_modules/tmux.nix
+++ home_modules/tmux.nix
@@ -2,10 +2,13 @@
   programs.tmux = {
     enable = true;
     clock24 = true;
+    keyMode = "vi";
     extraConfig = ''
       set -g prefix2 C-a
-      set -g status-style 'bg=#1f1f1f,fg=#dfdfdf'
+      set -g default-terminal "screen-256color"
+      set -g status-style 'bg=default,fg=#dfdfdf'
       set -g window-status-current-style 'bg=#007fff,fg=#dfdfdf'
+      set -s escape-time 10  # faster command sequences
     '';
   };
 }
