73147dd7779fe17b60fbda4df34c716793e18b2b
gorynich <terr2048@gmail.com>
2025-04-01 17:59:25 +0300

    tmux: fix italics

--- home_modules/tmux.nix
+++ home_modules/tmux.nix
@@ -5,6 +5,7 @@
     keyMode = "vi";
     extraConfig = ''
       set -g prefix2 C-a
+      set -g default-terminal "screen-256color"
       set -g status-style 'bg=default,fg=white'
       set -g window-status-current-style 'bg=blue,fg=black'
       set -s escape-time 10  # faster command sequences
