be48729049ee9565b2c045d3a82c50307229fde8
gorynich <terr2048@gmail.com>
2025-05-22 21:57:09 +0300

    xcore: remove xexebe ssh key, add corvus

--- machines/xcore/ssh.nix
+++ machines/xcore/ssh.nix
@@ -11,8 +11,9 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANY37jZd9CA4E2ktVrexTOochSow1yE4NYfCUB74fDC gorynich"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdqc3JqQub+9WygQnWPN5nGHHrZKgMdqAKA/E5haBR0 pozitive"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaUSEyu9MV6OjaMsETniGP3UxdbMjnkO41BfJxh+uwQ xexebe"
+      # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaUSEyu9MV6OjaMsETniGP3UxdbMjnkO41BfJxh+uwQ xexebe"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHuq4fzivJzFmOJV6xvLsjMELdtWOORogsD4jzIZBQIQ ospx"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkcU6BsD9ONs1I2iMkwOAPBP4O2f2Ox9C6CcclAS6c0 corvus"
     ];
   };
 }
