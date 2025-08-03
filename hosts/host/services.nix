62b013dc69922a01dd6fd09b7a2dd1631d5b842a
gorynich <terr2048@gmail.com>
2025-08-04 01:26:09 +0300

    host: add continue-vm

--- hosts/host/services.nix
+++ hosts/host/services.nix
@@ -29,5 +29,23 @@
           -vnc :0
       '';
     };
+    continue-vm = {
+      wantedBy = [ "multi-user.target" ];
+      restartIfChanged = false;
+      script = ''
+        VM_NAME=continue
+        VM_STORAGE=/var/vm/$VM_NAME
+        mkdir -p $VM_STORAGE
+        cd $VM_STORAGE
+        exec ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 \
+          -enable-kvm \
+          -m 2048 \
+          -smp 2 \
+          -hda continue-vm.qcow2 \
+          -nic tap,script=no,downscript=no,vhost=on,model=virtio-net-pci,ifname=vm4,mac=00:00:00:00:00:04 \
+          -device virtio-balloon,free-page-reporting=on \
+          -nographic
+      '';
+    };
   };
 }
