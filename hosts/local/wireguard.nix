42c061969c53d37c9af5c046aebb1fe0f0ef1be3
gorynich <terr2048@gmail.com>
2025-08-08 19:14:28 +0300

    simplify wireguard-route service

--- hosts/local/wireguard.nix
+++ hosts/local/wireguard.nix
@@ -21,18 +21,14 @@
     wireguard-route = {
       requiredBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
-      serviceConfig = let
-        ip = "${pkgs.iproute2}/bin/ip";
-      in {
+      path = with pkgs; [ iproute2 ];
+      serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = "yes";
-        ExecStart = pkgs.writers.writeBash "wg-up" ''
-          ${ip} route add 138.201.221.18 via 192.168.1.1
-        '';
-        ExecStop = pkgs.writers.writeBash "wg-down" ''
-          ${ip} route del 138.201.221.18 via 192.168.1.1
-        '';
       };
+      script = ''
+        ip route add 138.201.221.18 via 192.168.1.1
+      '';
     };
   };
 }
