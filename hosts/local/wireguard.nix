131d7d02a46eace77b62fc415a71308ce90394bf
gorynich <terr2048@gmail.com>
2025-10-18 14:21:34 +0300

    fix wg yet again

--- hosts/local/wireguard.nix
+++ hosts/local/wireguard.nix
@@ -6,6 +6,7 @@
   networking.wireguard.interfaces.wg0 = {
     ips = [ "10.0.0.2/24" ];
     listenPort = 62048;
+    # socketNamespace = "wg";
 
     peers = [
       {
@@ -20,6 +21,7 @@
   systemd.services = {
     wireguard-route = {
       wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" ];
       requires = [ "network-online.target" ];
       path = with pkgs; [ iproute2 ];
       serviceConfig = {
@@ -27,8 +29,24 @@
         RemainAfterExit = "yes";
       };
       script = ''
-        ip route add 138.201.221.18 via 192.168.1.1 || true
+        ip route add 138.201.221.18 via 192.168.1.1
       '';
     };
+
+    # wireguard-ns = {
+    #   requiredBy = [ "network-pre.target" ];
+    #   path = with pkgs; [ iproute2 ];
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #     RemainAfterExit = "yes";
+    #   };
+    #   script = ''
+    #     ip netns add wg
+    #     ip link add link enp4s0 name en0 netns wg type macvlan
+    #     ip -n wg link set en0 up
+    #     ip -n wg addr add 192.168.1.200/24 dev en0
+    #     ip -n wg route add 138.201.221.18 via 192.168.1.1
+    #   '';
+    # };
   };
 }
