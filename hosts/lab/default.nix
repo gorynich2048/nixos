27e8ad7e5d2e3ed9996b1ccbf035098350c0eadd
gorynich <terr2048@gmail.com>
2026-05-10 12:03:42 +0300

    add ssh key to lab

--- hosts/lab/default.nix
+++ hosts/lab/default.nix
@@ -30,7 +30,8 @@
     isNormalUser = true;
     linger = true;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxeqokMvKl1per8bjkqqrUBL14ETDsm7lwSS2B2gphJ root@wsl"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPe92LGBClSeLpArZLDjFGzg5LK8G6pA3TQ4RaNszRa root@wsl-laptop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxeqokMvKl1per8bjkqqrUBL14ETDsm7lwSS2B2gphJ root@wsl-azure"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANY37jZd9CA4E2ktVrexTOochSow1yE4NYfCUB74fDC gorynich"
       # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINW4tucb0u13bGb/1hB3GBo69z9XbK0Y6cRK4ZOP3t/E valer@Vfok-PC"
       # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAr87Ijv/4gAllpnZrhaQZhW+iI/HzAMEjaxFsbrA91L continue"
