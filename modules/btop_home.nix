8bc813a77c6fc8a5bb81c80f0b4b903666e9b012
gorynich <terr2048@gmail.com>
2026-05-10 19:08:06 +0300

    btop: fix gpu display

--- modules/btop_home.nix
+++ modules/btop_home.nix
@@ -1,13 +1,14 @@
-{
+{ pkgs, ... }: {
   programs.btop = {
     enable = true;
+    package = pkgs.btop-cuda;
     settings = {
       rounded_corners = false;
       theme_background = false;
-      show_gpu_info = "On";
-      show_coretemp = false;
+      show_coretemp = false; # broken
       cpu_graph_upper = "user";
       cpu_graph_lower = "system";
+      shown_boxes = "cpu mem net proc gpu0";
       update_ms = 100;
     };
   };
