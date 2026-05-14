93c37175bb2b3130518096e39506007f1f8b0259
gorynich <terr2048@gmail.com>
2026-05-14 20:13:46 +0300

    move zathura to local

--- hosts/local/programs.nix
+++ hosts/local/programs.nix
@@ -60,6 +60,19 @@
           font_size 12
         '';
       };
+
+      zathura = {
+        enable = true;
+        mappings = {
+          "l" = "scroll half-up";
+          "h" = "scroll half-down";
+        };
+        options = {
+          recolor = true;
+          recolor-darkcolor = "#ffffff";
+          recolor-lightcolor = "#1f1f1f";
+        };
+      };
     };
   };
 }
